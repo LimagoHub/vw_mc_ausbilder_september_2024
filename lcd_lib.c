b1359224f0d5141ee4cafac09fbf1332996ab94d
Jo Wagner <jowagner@limago.de>
2024-09-27 11:42:27 +0200

    initial commit

--- lcd_lib.c
+++ lcd_lib.c
@@ -1,27 +1,22 @@
-﻿
+﻿#include <stdlib.h>
 #include "lcd_lib.h"
-
-
-void LCD4_enable()
-{
-	LCD_DATA |= (1<<en);
-	_delay_us(1);
-	LCD_DATA &= ~(1<<en);
-	_delay_us(200);
-}
 
 void LCD4_cmd(unsigned char cmd)
 {
 	// Send upper nibble
 	LCD_DATA = (LCD_DATA & 0x0F) | (cmd & 0xF0);
 	LCD_DATA &= ~(1<<rs);  // RS = 0 for command
-	
-	LCD4_enable();
+	LCD_DATA |= (1<<en);
+	_delay_us(1);
+	LCD_DATA &= ~(1<<en);
+	_delay_us(200);
 	
 	// Send lower nibble
 	LCD_DATA = (LCD_DATA & 0x0F) | ((cmd << 4) & 0xF0);
-	
-	LCD4_enable();
+	LCD_DATA |= (1<<en);
+	_delay_us(1);
+	LCD_DATA &= ~(1<<en);
+	_delay_ms(2);
 }
 
 void LCD4_write(unsigned char data)
@@ -29,13 +24,17 @@ void LCD4_write(unsigned char data)
 	// Send upper nibble
 	LCD_DATA = (LCD_DATA & 0x0F) | (data & 0xF0);
 	LCD_DATA |= (1<<rs);  // RS = 1 for data
-	
-	LCD4_enable();
+	LCD_DATA |= (1<<en);
+	_delay_us(1);
+	LCD_DATA &= ~(1<<en);
+	_delay_us(200);
 	
 	// Send lower nibble
 	LCD_DATA = (LCD_DATA & 0x0F) | ((data << 4) & 0xF0);
-	
-	LCD4_enable();
+	LCD_DATA |= (1<<en);
+	_delay_us(1);
+	LCD_DATA &= ~(1<<en);
+	_delay_ms(2);
 }
 
 
@@ -45,24 +44,23 @@ void LCD4_init()
 	_delay_ms(20);   // Wait for LCD to power up
 	
 	// Initialize in 4-bit mode
-	LCD4_cmd(DDRAMADRESS_1);
+	LCD4_cmd(0x33);
 	_delay_ms(5);
-	LCD4_cmd(DDRAMADRESS_2);
+	LCD4_cmd(0x32);
 	_delay_ms(2);
-	LCD4_cmd(TWO_LINES);  // 4-bit mode, 2 lines, 5x8 font
+	LCD4_cmd(0x28);  // 4-bit mode, 2 lines, 5x8 font
 	_delay_ms(2);
-	LCD4_cmd(DISPLAYon);  // Display on, cursor off
+	LCD4_cmd(0x0C);  // Display on, cursor off
 	_delay_ms(2);
-	LCD4_cmd(INSERT_MODE);  // Increment cursor
+	LCD4_cmd(0x06);  // Increment cursor
 	_delay_ms(2);
-	LCD4_cmd(CLEAR);  // Clear display
+	LCD4_cmd(0x01);  // Clear display
 	_delay_ms(2);
 }
 
 
 void LCD4_write_string(const char *data)
 {
-	
 	int n=0;
 	while(data[n])
 	{
@@ -70,25 +68,41 @@ void LCD4_write_string(const char *data)
 		n++;
 		if (n==16)
 		{
-			LCD4_cmd(NEXT_LINE);
+			LCD4_cmd(0xC0);
 		}
 	}
 }
 
 void LCD4_gotoxy(int x, int y)
 {
-	
-	if (y==0)
+	y--;
+	if (x==1)
 	{
-		LCD4_cmd(0x80+x);
+		LCD4_cmd(0x80+y);
 	}
-	if (y==1)
+	if (x==2)
 	{
-		LCD4_cmd(SECOND_LINE+x);
+		LCD4_cmd(0xC0+y);
 	}
 }
 
 void LCD4_clear()
 {
-	LCD4_cmd(CLEAR);
+	LCD4_cmd(0x01);
+}
+
+void LCD4_write_int(int data){
+	const int datenbasisDezimal = 10;
+	char buffer [50];
+	itoa(data, buffer, datenbasisDezimal );
+	LCD4_write_string(buffer);
+}
+
+void LCD4_write_double(double data){
+	const int mindestZahlDerStellen = 1;
+	const int nachkommastellen = 2;
+	char buffer [50];
+	
+	dtostrf(data, mindestZahlDerStellen, nachkommastellen, buffer);
+	LCD4_write_string(buffer);
 }
--- lcd_lib.h
+++ lcd_lib.h
@@ -10,18 +10,6 @@
 
 #define F_CPU 16000000UL
 
-#define CLEAR 0b00000001
-#define INSERT_MODE 0b00000110
-#define TWO_LINES 0b00101000
-#define DISPLAYon 0b00001100
-#define DDRAMADRESS_1 0b00110011
-#define DDRAMADRESS_2 0b00110010
-#define OFF 0b00001000
-#define ON 0b00001110
-#define NEXT_LINE 0b11000000
-#define SECOND_LINE 0b11000000
-#define FIRST_LINE 0b10000000
-
 #include <avr/io.h>
 #include <util/delay.h>
 
